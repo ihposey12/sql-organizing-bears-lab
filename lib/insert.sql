0b6216fc51db40b7be189262bed35757c3b4e473
Hunter Posey <ihposey@crimson.ua.edu>
2020-11-27 20:24:31 -0800

    final procedure

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 12, "F", "Black", "Aggressive", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 8, "M", "Brown", "Calm", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 9, "M", "Brown", "Intense", 1);
+INSERT INTO bears (id, name, age ,sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 6, "F", "Black", "Nice", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Meliisa", 15, "F", "Black", "Aggressive", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 7, "M", "Black", "Slimy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 4, "F", "Brown", "Goofy", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, null, 20, "M", "Brown", "Nice", 0);
